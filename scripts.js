40cddd63a3c89a484d8d9b7aea32679c1786ad5b
GevinNoone <gevinnoone1020@gmail.com>
2021-12-03 16:12:28 -0500

    Website
    First Website

---
+++ scripts.js
@@ -0,0 +1,7 @@
+const name = document.querySelector(".fb",".insta");
+function toFacebook(){
+    window.open("https://www.facebook.com/The-Pastry-Box-686169974758243", "_blank");
+}
+function toInstagram(){
+    window.open("https://www.instagram.com/thepastryboxplainville/", "_blank");
+}
